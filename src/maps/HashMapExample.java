b1f076422ccb4aad8a596990cc5d8c27cb017474
Chandan K M <chandan.km@tecnotree.com>
2024-08-21 22:21:35 +0530

    added hash map code

--- src/maps/HashMapExample.java
+++ src/maps/HashMapExample.java
@@ -1,6 +1,7 @@
 package maps;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class HashMapExample {
     public static void main(String[] args) {
@@ -17,5 +18,21 @@ public class HashMapExample {
         map1.put(105,"Banana");
         map1.put(105,"Orange");// overirdes banana and prints organe
         System.out.println("map1 is : "+map1);
+
+        System.out.println("map1 size is  : "+map1.size());
+
+        // remove data
+        map1.remove(104);
+        System.out.println("after removing one element map1 is : "+map1);
+
+        // print specific element
+        System.out.println("element of 103 key is  : "+map1.get(103));
+
+
+        // traversing the hashmap using foreach
+        System.out.println("Traversing map1 using foreach loop");
+        for (Map.Entry m1 : map1.entrySet()){
+            System.out.println(m1.getKey()+"--------"+m1.getValue());
+        }
     }
 }
